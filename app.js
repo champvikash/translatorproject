08d53df59864e33099ceaadbe98b26065be8930b
VIKASH KUMAR <champvikash4@gmail.com>
2022-12-06 16:43:37 +0530

    commit 2nd

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+// input
+var username = prompt("what is your name?");
+
+//output
+var welcomeMessage =  ("this script is work welcome to Delhi " + username);
+
+alert(welcomeMessage);
+
